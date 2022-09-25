6890bdd6157044c0fdda6cdb4a4d77a92b90936a
Patryk Lezon <Plezo@protonmail.com>
2022-09-25 00:29:22 -0400

    Solved PPER

--- PPER.py
+++ PPER.py
@@ -1,11 +1,27 @@
-from math import factorial
+from utils import *
 
 def solve(ds):
-    n, k = [int(i) for i in ds[0].split(' ')]
-    
-    print(int((factorial(n) / (factorial(n-k))) % 1000000))
+    dna = list(ds.values())[0]
+
+    for i in range(len(dna)):
+        sub = ''
+        for j in range(12):
+
+            if i + j >= len(dna):
+                break
+
+            sub += dna[i+j]
+
+            if sub == getCompliment(sub):
+                if 4 <= len(sub) <= 12:
+                    print(i+1, j+1)
+
+
 
 if __name__ == '__main__':
     f = open('ds.txt', 'r')
     ds = [line.strip() for line in f]
+
+    ds = getFASTADict(ds)
+
     solve(ds)
